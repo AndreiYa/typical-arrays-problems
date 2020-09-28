2ad84326884a0306fe19ccb68ae01b7a923337a1
AndreiYa <newminer@bk.ru>
2020-09-28 19:43:45 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!!array && array != '') {
+    array.sort((a, b) => {
+        return a - b;
+    });
+    return array[0];
+  } else {
+      return 0;
+  } 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!!array && array != '') {
+    array.sort((a, b) => {
+        return a - b;
+    });
+    array.reverse();
+    return array[0];
+  } else {
+      return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!!array && array != '') {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else {
+        return 0;
+    }
 }
